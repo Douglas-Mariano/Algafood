a9aec66e3b9d0171b90e07867383013af4db30f6
Douglas Mariano <douglas.arm@live.com>
2024-02-21 10:23:16 -0300

    feat: Adicionando content negotiation para poder retornar xml e json

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +12,13 @@ import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
 @RestController
-@RequestMapping("/cozinhas")
+@RequestMapping("/cozinhas") // produces = MediaType.APPLICATION_JSON_VALUE) o produces pode ser passado para todos os metodos porém tem que colocar o value= "/cozinhas"
 public class CozinhaController {
 
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 	
-	@GetMapping
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Cozinha> listar() {
 		return cozinhaRepository.listar();
 	}
