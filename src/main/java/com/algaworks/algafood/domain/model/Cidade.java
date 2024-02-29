9646a53d39e16685f8b05657fbd5de090878a542
Douglas Mariano <douglas.arm@live.com>
2024-02-29 16:49:57 -0300

    feat: Criando classes mixin para usar as anotações do Jackson

--- src/main/java/com/algaworks/algafood/domain/model/Cidade.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cidade.java
@@ -14,8 +14,6 @@ import javax.validation.groups.ConvertGroup;
 import javax.validation.groups.Default;
 
 import com.algaworks.algafood.core.validation.Groups;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -33,7 +31,6 @@ public class Cidade {
 	@Column(nullable = false)
 	private String nome;
 	
-	@JsonIgnoreProperties(value = "nome", allowGetters = true)
 	@Valid
 	@ConvertGroup(from = Default.class, to = Groups.EstadoId.class)
 	@NotNull
