d5053a62472b694b652069d20596790c34df2472
Douglas-Mariano <douglas.arm@live.com>
2024-03-21 16:58:29 -0300

    feat: Iniciando a configuração da documentação com springdoc

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
@@ -2,9 +2,13 @@ package com.algaworks.algafood.api.v1.openapi.controller;
 
 import com.algaworks.algafood.api.v1.model.CidadeModel;
 import com.algaworks.algafood.api.v1.model.input.CidadeInput;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 
+@SecurityRequirement(name = "security_auth")
+@Tag(name = "Cidades")
 public interface CidadeControllerOpenApi {
 
 	CollectionModel<CidadeModel> listar();
@@ -16,5 +20,5 @@ public interface CidadeControllerOpenApi {
 	CidadeModel atualizar(Long cidadeId, CidadeInput cidadeInput);
 
 	ResponseEntity<Void> remover(Long cidadeId);
-	
+
 }
--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/GrupoControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/GrupoControllerOpenApi.java
@@ -2,9 +2,11 @@ package com.algaworks.algafood.api.v1.openapi.controller;
 
 import com.algaworks.algafood.api.v1.model.GrupoModel;
 import com.algaworks.algafood.api.v1.model.input.GrupoInput;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 
+@SecurityRequirement(name = "security_auth")
 public interface GrupoControllerOpenApi {
 
 	CollectionModel<GrupoModel> listar();
