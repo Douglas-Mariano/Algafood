1340e0313f57e7215da5f0a2d5c619ddc7623703
Douglas-Mariano <douglas.arm@live.com>
2024-03-21 10:35:53 -0300

    feat: finalizando Docker

--- src/main/java/com/algaworks/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
@@ -20,6 +20,7 @@ import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
 import org.springframework.security.oauth2.provider.approval.ApprovalStore;
 import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
+import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
@@ -36,89 +37,79 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
 	@Autowired
 	private AuthenticationManager authenticationManager;
-	
+
 	@Autowired
 	private UserDetailsService userDetailsService;
-	
+
 	@Autowired
 	private JwtKeyStoreProperties jwtKeyStoreProperties;
-	
+
 	@Autowired
 	private DataSource dataSource;
-	
+
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.jdbc(dataSource);
 	}
-	
+
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 //		security.checkTokenAccess("isAuthenticated()");
-		security.checkTokenAccess("permitAll()")
-			.tokenKeyAccess("permitAll()")
-			.allowFormAuthenticationForClients();
+		security.checkTokenAccess("permitAll()").tokenKeyAccess("permitAll()").allowFormAuthenticationForClients();
 	}
-	
+
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		var enhancerChain = new TokenEnhancerChain();
-		enhancerChain.setTokenEnhancers(
-				Arrays.asList(new JwtCustomClaimsTokenEnhancer(), jwtAccessTokenConverter()));
-		
-		endpoints
-			.authenticationManager(authenticationManager)
-			.userDetailsService(userDetailsService)
-			.reuseRefreshTokens(false)
-			.accessTokenConverter(jwtAccessTokenConverter())
-			.tokenEnhancer(enhancerChain)
-			.approvalStore(approvalStore(endpoints.getTokenStore()))
-			.tokenGranter(tokenGranter(endpoints));
+		enhancerChain.setTokenEnhancers(Arrays.asList(new JwtCustomClaimsTokenEnhancer(), jwtAccessTokenConverter()));
+
+		endpoints.authenticationManager(authenticationManager).userDetailsService(userDetailsService)
+				.authorizationCodeServices(new JdbcAuthorizationCodeServices(this.dataSource)).reuseRefreshTokens(false)
+				.accessTokenConverter(jwtAccessTokenConverter()).tokenEnhancer(enhancerChain)
+				.approvalStore(approvalStore(endpoints.getTokenStore())).tokenGranter(tokenGranter(endpoints));
 	}
-	
+
 	private ApprovalStore approvalStore(TokenStore tokenStore) {
 		var approvalStore = new TokenApprovalStore();
 		approvalStore.setTokenStore(tokenStore);
-		
+
 		return approvalStore;
 	}
-	
+
 	@Bean
 	public JWKSet jwkSet() {
-		RSAKey.Builder builder = new RSAKey.Builder((RSAPublicKey) keyPair().getPublic())
-				.keyUse(KeyUse.SIGNATURE)
-				.algorithm(JWSAlgorithm.RS256)
-				.keyID("algafood-key-id");
-		
+		RSAKey.Builder builder = new RSAKey.Builder((RSAPublicKey) keyPair().getPublic()).keyUse(KeyUse.SIGNATURE)
+				.algorithm(JWSAlgorithm.RS256).keyID("algafood-key-id");
+
 		return new JWKSet(builder.build());
 	}
-	
+
 	@Bean
 	public JwtAccessTokenConverter jwtAccessTokenConverter() {
 		var jwtAccessTokenConverter = new JwtAccessTokenConverter();
 		jwtAccessTokenConverter.setKeyPair(keyPair());
-		
+
 		return jwtAccessTokenConverter;
 	}
-	
+
 	private KeyPair keyPair() {
 		var keyStorePass = jwtKeyStoreProperties.getPassword();
 		var keyPairAlias = jwtKeyStoreProperties.getKeypairAlias();
-		
-		var keyStoreKeyFactory = new KeyStoreKeyFactory(
-				jwtKeyStoreProperties.getJksLocation(), keyStorePass.toCharArray());
-		
+
+		var keyStoreKeyFactory = new KeyStoreKeyFactory(jwtKeyStoreProperties.getJksLocation(),
+				keyStorePass.toCharArray());
+
 		return keyStoreKeyFactory.getKeyPair(keyPairAlias);
 	}
-	
+
 	private TokenGranter tokenGranter(AuthorizationServerEndpointsConfigurer endpoints) {
 		var pkceAuthorizationCodeTokenGranter = new PkceAuthorizationCodeTokenGranter(endpoints.getTokenServices(),
 				endpoints.getAuthorizationCodeServices(), endpoints.getClientDetailsService(),
 				endpoints.getOAuth2RequestFactory());
-		
-		var granters = Arrays.asList(
-				pkceAuthorizationCodeTokenGranter, endpoints.getTokenGranter());
-		
+
+		var granters = Arrays.asList(pkceAuthorizationCodeTokenGranter, endpoints.getTokenGranter());
+
 		return new CompositeTokenGranter(granters);
 	}
-	
+
 }
